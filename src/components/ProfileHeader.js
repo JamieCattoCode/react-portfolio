18f11ed7cb2ff4bb31980fe65dd9d00f54b0083f
Jamie Catto <jamietcatto@gmail.com>
2023-04-04 15:35:47 +0200

    Added copy instead of lorem

--- src/components/ProfileHeader.js
+++ src/components/ProfileHeader.js
@@ -1,11 +1,14 @@
+import copy from '../utils/copy.json'
+
 const ProfileHeader = () => {
+    const { profileHeader } = copy
     return (
         <div className='text-center p-10'>
             <h2 className='text-5xl py-2 text-teal-600 font-medium'>
-                Jamie Catto
+                {profileHeader.mainTitle}
             </h2>
-            <h3 className='text-xl py-3 dark:text-slate-400'>Full Stack Developer</h3>
-            <p className='text-base leading-6 text-gray-800 dark:text-slate-500'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus maximus diam non.</p>
+            <h3 className='text-xl py-3 dark:text-slate-400'>{profileHeader.subTitle}</h3>
+            <p className='text-base leading-6 text-gray-800 dark:text-slate-500'>{profileHeader.subSubTitle}</p>
         </div>
     )
 }
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,17 +1,17 @@
 import skills from '../utils/skills.json'
 import SkillCard from './SkillCard'
+import copy from '../utils/copy.json'
 
 const Skills = () => {
     const { cards } = skills
+    const { skillsSection } = copy
     
     return (
         <div className='py-10'>
             <h3 className='text-xl font-medium text-center p-1 lg:text-3xl lg:mb-5 dark:text-teal-600'>My Skills</h3>
             <div className='flex justify-center px-4'>
                 <p className='text-base text-center max-w-screen-md dark:text-slate-400 lg:text-lg'>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Orci sagittis eu 
-                    volutpat odio facilisis mauris. Condimentum vitae sapien pellentesque habitant morbi tristique senectus et netus. Ultrices in iaculis nunc 
-                    sed augue lacus. Aenean sed adipiscing diam donec adipiscing tristique risus.
+                    {skillsSection.summaryParagraph}
                 </p>
             </div>
             <div className='lg:flex flex-wrap gap-10 lg:p-10'>
