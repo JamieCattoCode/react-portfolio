ad210d6e49dd22fcb1019d6d5170a90e39a63384
Jamie Catto <jamietcatto@gmail.com>
2023-04-09 11:36:16 +0200

    Hosted profile picture on a server

--- src/components/CirclePicture.js
+++ src/components/CirclePicture.js
@@ -4,7 +4,7 @@ const CirclePicture = () => {
     return (
         <div className='flex justify-center p-9'>
             <Image 
-            src="/../public/profile-pic.jpg"
+            src="https://i.ibb.co/ydS3J0v/profile-pic.jpg"
             width={200}
             height={200} 
             className='rounded-full w-200 h-200 lg:w-96 lg:h-96'
